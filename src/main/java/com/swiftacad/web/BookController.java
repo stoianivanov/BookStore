aed665ee096704db72810faeb99cff9f15e4f0ab
Stoian Ivanov <stoianivanovivanov@abv.bg>
2019-12-06 07:01:21 +0000

    add cross origins

--- src/main/java/com/swiftacad/web/BookController.java
+++ src/main/java/com/swiftacad/web/BookController.java
@@ -9,6 +9,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ public class BookController {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value="/books", method= RequestMethod.GET)
 	public ResponseEntity<List<Book>> findAll(){
 		List<Book> books = new ArrayList<>();
